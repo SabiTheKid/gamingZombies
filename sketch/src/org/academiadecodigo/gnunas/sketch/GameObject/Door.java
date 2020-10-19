bd84ab31bfcbacb483523443cc989bdfd1d0099e
pablogbarahona <pablogbarahona@gmail.com>
2020-10-19 18:20:07 +0100

    "Door and key implemented"

--- sketch/src/org/academiadecodigo/gnunas/sketch/GameObject/Door.java
+++ sketch/src/org/academiadecodigo/gnunas/sketch/GameObject/Door.java
@@ -5,7 +5,15 @@ import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class Door extends GameObject{
+    private boolean opened;
     public Door(Position pos) {
         super(pos, new Picture());
+    }
+
+    public void openDoor(){
+        opened = true;
+        super.getPos().moveUp();
+        super.getPicture().load(/*adress of the new image*/);
+
     }
 }
